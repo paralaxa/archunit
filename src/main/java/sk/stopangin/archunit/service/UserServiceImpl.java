71bc1151416d218fd9486a5279976c0149f21a06
stopAngin <stop@angin.com>
2023-01-29 15:44:32 +0100

    using toList() instead of explicit collector to make sonarlint happy

--- src/main/java/sk/stopangin/archunit/service/UserServiceImpl.java
+++ src/main/java/sk/stopangin/archunit/service/UserServiceImpl.java
@@ -25,7 +25,7 @@ public class UserServiceImpl implements UserService {
 
   @Override
   public List<UserDto> getAll() {
-    return repository.findAll().stream().map(this::dtoFromUser).collect(Collectors.toList());
+    return repository.findAll().stream().map(this::dtoFromUser).toList();
   }
 
   private UserDto dtoFromUser(User user) {
